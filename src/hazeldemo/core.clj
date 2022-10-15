8fabe85bd1dd8620ada853ee5b27903c91fc7f8d
tom <tom@that-place>
2022-10-15 12:26:31 -0500

    Added client status for hface

--- src/hazeldemo/core.clj
+++ src/hazeldemo/core.clj
@@ -2,3 +2,19 @@
   (:require [chazel.core :as ch]))
 
 (ch/cluster-of 1 :name "dev")
+
+(def app1 (ch/hz-map :app1))
+(defn ping []
+  (println "ping!"))
+
+(def this-ns *ns*)
+
+(defn interpret [args]
+  (binding [*ns* this-ns]
+    (eval (read-string args))))
+
+(ch/put! app1 :apple 42)
+
+#_
+(defn invoke-all [& args]
+  (.executeOnAllMembers exec-svc (Rtask. fun)))
